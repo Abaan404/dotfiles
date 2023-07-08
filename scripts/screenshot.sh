b0e56f959cc5a1b19aa4e97f848bfebdd6a1e1cd
Abaan404 <67100191+Abaan404@users.noreply.github.com>
2023-07-08 15:33:33 +0300

    dropshadow screenshots

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,13 +2,26 @@
 
 FILENAME=~/Pictures/screenshots/$(date +'%Y%m%d-%H%M%S_screenshot.png')
 
+# grab res
 if [[ $1 = "region" ]]; then
-    slurp -b 111111AA -c 111111AA | grim -g - $FILENAME
+    size=$(slurp -b 111111AA -c 111111AA)
 elif [[ $1 == "window" ]]; then
-    grim -g "$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" $FILENAME
+    size=$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
 elif [[ $1 == "screen" ]]; then
-    grim -g "$(hyprctl -j monitors | jq -r '.[] | "\(.x),\(.y) \(.width)x\(.height)"')" $FILENAME
+    size=$(hyprctl -j monitors | jq -r '.[] | "\(.x),\(.y) \(.width)x\(.height)"')
 fi
 
-notify-send "Screenshot $1 Captured" -i $FILENAME
+# capture screen
+grim -g "$size" $FILENAME
+
+# dropshadow
+convert $FILENAME \( +clone -background black -shadow 75x10+0+0 \) +swap -background none -layers merge +repage $FILENAME
+
+# copy to clipboard
 wl-copy < $FILENAME
+
+# notify
+action=$(dunstify --action="swappy,swappy" "Screenshot $1 Captured" -i $FILENAME)
+if [[ $action == "swappy" ]]; then
+    swappy -f $FILENAME
+fi
