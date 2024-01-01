f63649fab4065684853469ca9adac3e1835ec527
Abaan404 <67100191+Abaan404@users.noreply.github.com>
2024-01-01 12:11:06 -0600

    hyprland mute keybind fix

--- config/ags/ts/main.ts
+++ config/ags/ts/main.ts
@@ -1,5 +1,8 @@
 import App from "resource:///com/github/Aylur/ags/app.js";
 
+// services
+import Mpris from "resource:///com/github/Aylur/ags/service/audio.js";
+
 import { exec } from "resource:///com/github/Aylur/ags/utils.js";
 import { toggle_window, spawn_window } from "./window.js";
 
@@ -10,6 +13,12 @@ exec(`sass ${scss} ${css}`);
 
 // for usage outside the ags process (i.e. hyprland keybinds)
 globalThis.toggle = (name: string) => toggle_window(name);
+globalThis.mute = (type: "speaker" | "microphone") => {
+    if (type === "speaker" && Mpris.speaker)
+        Mpris.speaker.is_muted = !Mpris.speaker.is_muted
+    else if (type === "microphone" && Mpris.microphone)
+        Mpris.microphone.is_muted = !Mpris.microphone.is_muted
+}
 
 export default {
     style: App.configDir + "/style.css",
