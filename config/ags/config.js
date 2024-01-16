716e34d60c9e51bf554521ea66103c955b6a22f9
Abaan404 <67100191+Abaan404@users.noreply.github.com>
2024-01-16 02:09:23 -0600

    better transpiling

--- config/ags/config.js
+++ config/ags/config.js
@@ -1,39 +1,15 @@
 import App from "resource:///com/github/Aylur/ags/app.js"
 import { execAsync } from "resource:///com/github/Aylur/ags/utils.js"
 
-// cmake is better than this honestly
+const dir = "/tmp/ags/js";
 
-// this does mean it needs to recompile every
-// refresh but its soo fast that it shouldnt
-// even be noticable
+await execAsync([
+    "bun", "build", `${App.configDir}/ts/main.ts`,
+    "--outdir", dir,
+    "--external", "resource:///*",
+    "--external", "gi://*",
+    "--external", "cairo",
+]).catch(console.error);
 
-const dir = "/tmp/ags/js"
-const promises = [];
-const files = [
-    "/ts/main.ts",
-    "/ts/utils.ts",
-    "/ts/window.ts",
-    "/ts/variables.ts",
-    "/ts/windows/bar.ts",
-    "/ts/windows/powermenu.ts",
-    "/ts/windows/media.ts",
-    "/ts/windows/player.ts",
-];
-
-files.forEach(file => {
-    let outDir = dir;
-    if (file.startsWith("/ts/windows"))
-        outDir = `${dir}/windows`;
-
-    promises.push(
-        execAsync(["bun", "build",
-            App.configDir + file,
-            "--outdir", outDir,
-            "--external", "*",
-        ])
-    );
-})
-
-await Promise.all(promises);
 const main = await import(`file://${dir}/main.js`);
 export default main.default;
