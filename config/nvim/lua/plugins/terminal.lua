645da721a28ad04a103ac1a827d864ef7686324e
Abaan404 <67100191+Abaan404@users.noreply.github.com>
2024-02-02 19:09:32 -0600

    toggleterm :wqa hack

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -3,24 +3,22 @@ return {
         "akinsho/toggleterm.nvim",
         version = "*",
         config = function()
-            local toggleterm = require("toggleterm")
+            -- ugly way to allow :wqa without neovim getting angry
+            vim.api.nvim_create_autocmd({ "TermEnter" }, {
+                callback = function()
+                    for _, buffers in ipairs(vim.fn.getbufinfo()) do
+                        local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, "filetype")
+                        if filetype == "toggleterm" then
+                            vim.api.nvim_create_autocmd({ "BufWriteCmd", "FileWriteCmd", "FileAppendCmd" }, {
+                                buffer = buffers.bufnr,
+                                command = "q!",
+                            })
+                        end
+                    end
+                end,
+            })
 
-            -- -- ugly way to allow :wqa without neovim getting angry, might belong in a toggleterm PR
-            -- local close_terminals = function()
-            --     for _, buffers in ipairs(vim.fn.getbufinfo()) do
-            --         local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, 'filetype')
-            --         if filetype == 'toggleterm' then
-            --             vim.cmd("!logger " .. filetype)
-            --             vim.api.nvim_create_autocmd({ "BufWriteCmd", "FileWriteCmd", "FileAppendCmd" }, {
-            --                 buffer = buffers.bufnr,
-            --                 command = "bdelete! " .. buffers.bufnr
-            --             })
-            --         end
-            --     end
-            -- end
-            -- vim.api.nvim_create_autocmd({ "TermEnter" }, { callback = close_terminals })
-
-            toggleterm.setup({})
+            require("toggleterm").setup()
         end,
     },
 }
