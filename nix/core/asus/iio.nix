d87ad9b925245089a1d71b8a18d12a70b439a622
abaan404 <67100191+Abaan404@users.noreply.github.com>
2025-01-31 14:31:22 -0600

    nix: iio fix

--- nix/core/asus/iio.nix
+++ nix/core/asus/iio.nix
@@ -7,4 +7,15 @@
   programs.iio-hyprland = {
     enable = true;
   };
+
+  # accel_3d stops updating after a suspend, forcing a read causes it to update and work.. weird fix but ok
+  systemd.services.iio-poller = {
+    description = "Polls IIO accelerometer to force updates for auto-rotation";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "/bin/sh -c 'while true; do cat /sys/bus/iio/devices/iio:device*/in_accel_x_raw > /dev/null; sleep 2; done'";
+      Restart = "always";
+    };
+  };
 }
